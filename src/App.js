5812442fd289ab8e0986149005b918ee97ee986e
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-06 00:19:48 +0530

    v-4 from

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithPopup ,GoogleAuthProvider } from "firebase/auth";
+// import {  } from "firebase/auth";
 
 const auth = getAuth(app);
+
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () =>{
+     signInWithPopup(auth,provider)
+    .then(result =>{
+      const user = result.user;
+      console.log(user)
+    }).catch(error =>{
+      console.log(error,'error')
+    })
+
+  }
   return (
-    <div className="App">
-      
+    <div className="App"style={{'margin': '10px'}}>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
 
 export default App;
+
+
+
+
