b7ef34c4f42254d6d2824745a678f4d65a3197c0
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-06 02:11:33 +0530

    signIn signOut and toggle by tarnery v-6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 
+                                   "firebase/auth";
 import app from './firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user,setUser] = useState({});
+
   const provider = new GoogleAuthProvider();
+  // sign in by google 
   const handleGoogleSignIn = () =>{
        signInWithPopup(auth,provider)
        .then(result=>{
           const user = result.user
+          setUser(user);
           console.log(user);
        })
        .catch(error=>{
         console.log(error)
        })
   }
-  
+  // sign out by google 
+  const  handleGoogleSignOut = () =>{
+     signOut(auth)
+     .then( ()=>{
+         setUser({})
+     })
+     .catch( error=>{
+         setUser({})
+     })
+     }
   return (
     <div className="App"style={{'margin': '10px'}}>
-    <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.email ? <button onClick={handleGoogleSignOut}>Google Sign Out</button> : <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      }
+    
+    <h4>user : {user.displayName}</h4>
+    <h4>email : {user.email}</h4>
+    <img src={user.photoURL} alt="" />
     </div>
   );
 }
